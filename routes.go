f5a423f2d547daaba452063e741af7c3dfc72299
Ryan Stafford <ryan@nanorex.net>
2023-07-28 13:04:41 -0400

    remove mailto links from title

--- routes.go
+++ routes.go
@@ -152,7 +152,7 @@ var funcMap = template.FuncMap{
 			return body
 		}
 		text := html2text.HTML2TextWithOptions(buf.String(), html2text.WithLinksInnerText())
-		re := regexp.MustCompile(`\<https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)\>`)
+		re := regexp.MustCompile(`\<(https?:\/\/|mailto)(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)\>`)
 		return re.ReplaceAllString(text, "")
 	},
 	"contains": strings.Contains,
